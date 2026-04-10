4d922e61faeb2657b921b4d1210f99f38e265c3c
sarahtannous <sarahtannous777@gmail.com>
2026-04-10 14:43:53 -0400

    Removed SignOut file because we did not ended up using it.

--- nutrition-flow/app/components/SignOut.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useClerk } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-
-export function SignOutButton() {
-  const { signOut } = useClerk(); //sign out function from Clerk
-  const router = useRouter(); //router to redirect user after signing out
-}
